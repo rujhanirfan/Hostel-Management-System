88824acfad2f145140511bfac6658d67398f2ac8
rujhanirfan <55092041+rujhanirfan@users.noreply.github.com>
2022-06-08 15:28:13 +0530

    Update the Project to Github

---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+<footer class="footer text-center text-muted">
+&copy; <?php echo date("Y"); ?> - Hostel Management System - by <a href="https://freeprojectscodes.com">Freeprojectscodes</a>
+</footer>
